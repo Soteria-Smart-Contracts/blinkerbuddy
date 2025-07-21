81a61138817c3858f622af571c8b06755845f351
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2025-07-21 22:45:43 +0000

    Jul 21, 2025, 6:45 PM

--- browser_app/js/script.js
+++ browser_app/js/script.js
@@ -191,10 +191,11 @@ checknewday(); // Check if it's a new day to reset blink count
 document.addEventListener('DOMContentLoaded', () => {
    //the sotrage is checked for an id at the start of the script, so we can use that instead, and then call /loadusername:id to fetch the username of the user
    //if the id is not empty, prompt to enter username, oth
+   
     const blinkStats = document.getElementById('blink-stats');
     const tooltip = document.getElementById('username-tooltip');
 
-    if (username !== '') {
+    if (userId !== '') {
         tooltip.textContent = username;
     } else {
         document.getElementById('username-modal').style.display = 'flex';
