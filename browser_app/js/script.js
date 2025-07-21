d89f08769d3a822e7abd83ce74153ed27780d73e
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2025-07-21 22:49:02 +0000

    Jul 21, 2025, 6:49 PM

--- browser_app/js/script.js
+++ browser_app/js/script.js
@@ -202,7 +202,8 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .then(data => {
             console.log('Username loaded:', data);
-            //the data comes out as 
+            //the data comes out as { "id": "2307faa68da8836ec6264427e06d963b", "username": "john123", "blinkscore": 42 }, extract the username
+            const responseData = JSON.parse(data);
 
         })
         .catch(error => {
