6589a896c0bcb5f25864ab5b0b7971b82bf6ca98
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2025-07-21 22:41:24 +0000

    Jul 21, 2025, 6:41 PM

--- browser_app/js/script.js
+++ browser_app/js/script.js
@@ -190,7 +190,8 @@ checknewday(); // Check if it's a new day to reset blink count
 
 document.addEventListener('DOMContentLoaded', () => {
    // const username = localStorage.getItem('bbUsername');
-   //
+   //call the endpoint, get the username /loadusername:username
+   
     const blinkStats = document.getElementById('blink-stats');
     const tooltip = document.getElementById('username-tooltip');
 
