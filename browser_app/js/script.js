4af4f0c15223a6f47b033c9c247064878d3976dd
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2025-07-22 20:02:30 +0000

    Jul 22, 2025, 4:02 PM

--- browser_app/js/script.js
+++ browser_app/js/script.js
@@ -384,7 +384,6 @@ function startTimer(plot, index) {
                 highScore = totalBlinkersToday;
             }
             localStorage.setItem('treeStates', JSON.stringify(treeStates));
-            localStorage.setItem('highScore', highScore);
             console.log('Tree planted!');
             timerElement.textContent = 'Planted!';
             timerElement.style.fontSize = '16px';
