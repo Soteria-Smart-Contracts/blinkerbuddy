74f6fedc4dd77c727b352523389bc14a38825d57
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2025-07-22 21:07:14 +0000

    Jul 22, 2025, 5:07 PM

--- browser_app/js/script.js
+++ browser_app/js/script.js
@@ -283,7 +283,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     document.getElementById('username-modal').style.display = 'none'; // Hide the modal
                     //it now also returns treeStates, so we can load them       treeState: userData.treeState || []
                     //the treestate is a string which needs to be parsed into an array, one liner, i know its a string already
-                    newtreeStates = data.treeState ? JSON.parse(data.treeState) : []; // Parse tree states from string
+                    newtreeStates = data.treeStates ? JSON.parse(data.treeState) : []; // Parse tree states from string
                     treeStates = newtreeStates; // Assign to global treeStates
                     console.log('Tree states loaded:', treeStates);
                     updatePlots(); // Update the plots with loaded tree states
