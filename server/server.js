7a5fa6b04efa052dcca77ced567531d801488366
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2025-07-23 00:14:08 +0000

    Jul 22, 2025, 8:14 PM

--- server/server.js
+++ server/server.js
@@ -1,29 +1,2 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
 
-// Enable CORS for all origins including Netlify
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: false
-}));
 
-// Handle keepalive endpoint
-app.get('/keepalive', (req, res) => {
-    res.set('Content-Type', 'text/plain');
-    res.send('OK');
-});
-
-app.listen(process.env.PORT || 5000, '0.0.0.0', () => {
-    console.log(`Keepalive server running on port ${process.env.PORT || 5000}`);
-});
-
-//how to fetch http://localhost:5000/keepalive from console?
-//// You can use the fetch API in the browser console or any HTTP client like Postman.
-// Example using fetch API in the browser console:
-// fetch('http://localhost:5000/keepalive')
-//     .then(response => response.text())
-//     .then(data => console.log(data))
-// Example using Postman:
